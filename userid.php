b57504aa0cae0cfeebcafff61e8e9e33195d3d90
Thomas ten Cate <ttencate@gmail.com>
2016-07-24 19:35:43 +0200

    Sanitize event_id and query params for autocomplete (#3)

--- userid.php
+++ userid.php
@@ -4,8 +4,8 @@ require_once(__DIR__ . '/includes/init.php');
 
 $db = db_connect();
 
-$event_id = $_GET['event'];
-$query = $_GET['query'];
+$event_id = util_sanitize_query_param('event');
+$query = util_sanitize_query_param('query');
 
 $sql = "SELECT uid, author FROM entry WHERE event_id = '$event_id' AND author LIKE '%$query%' ORDER BY author LIMIT 20;";
 $results = mysqli_query($db, $sql) or log_error_and_die('Failed to fetch username', mysqli_error($db)); 
