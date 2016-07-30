fb6b4b3ccd0ed11cb95699891bb599673a398091
Marwane Kalam-Alami <marwane.ka@gmail.com>
2016-07-30 16:55:18 +0200

    create db_query function to reduce verbose, start by using it in setting.php (#3)

--- includes/setting.php
+++ includes/setting.php
@@ -2,38 +2,26 @@
 
 function setting_read($db, $id, $default_value = null) {
 	$ret_val = $default_value;
-	$stmt = mysqli_prepare($db, "SELECT value FROM setting WHERE id = ?");
-	mysqli_stmt_bind_param($stmt, 's', $id);
-	mysqli_stmt_execute($stmt);
-	if ($results = mysqli_stmt_get_result($stmt)) {
-		if (mysqli_num_rows($results) > 0) {
-			$setting = mysqli_fetch_array($results);
-			$ret_val = $setting['value'];
-		}
+	$results = db_query($db, "SELECT value FROM setting WHERE id = ?", 's', $id);
+	if (count($results) > 0) {
+		$setting = $results[0];
+		$ret_val = $setting['value'];
 	}
-	mysqli_stmt_close($stmt);
 	return $ret_val;
 }
 
 function setting_write($db, $id, $value) {
 	if (setting_read($db, $id, null) === null) {
-		$stmt = mysqli_prepare($db, "INSERT INTO setting(id,value) VALUES(?, ?)");
-		mysqli_stmt_bind_param($stmt, 'ss', $id, $value);
-		if(!mysqli_stmt_execute($stmt)){
-			mysqli_stmt_close($stmt);
+		if (!db_query($db, "INSERT INTO setting(id,value) VALUES(?, ?)",
+				'ss', $id, $value)) {
 			die("Failed to insert setting '$id' with value '$value'");
 		}
-		mysqli_stmt_close($stmt);
 	}
 	else {
-
-		$stmt = mysqli_prepare($db, "UPDATE setting SET value = ? WHERE id = ?");
-		mysqli_stmt_bind_param($stmt, 'ss', $value, $id);
-		if(!mysqli_stmt_execute($stmt)){
-			mysqli_stmt_close($stmt);
-			die("Failed to update setting '$id' with value '$value'");
+		if (!db_query($db, "UPDATE setting SET value = ? WHERE id = ?",
+				'ss', $value, $id)) {
+			die("Failed to insert setting '$id' with value '$value'");
 		}
-		mysqli_stmt_close($stmt);
 	}
 }
 
